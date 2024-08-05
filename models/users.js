40c01dff179cf2b11eabf550c07f25ac1340c107
Franek-86 <fraleccese86@gmail.com>
2024-08-05 19:03:55 +0200

    test

--- models/users.js
+++ models/users.js
@@ -7,19 +7,10 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide name"],
     // maxlength: 50,
-    minlength: 3,
-    maxlength: 6,
+    minlength: 2,
+    maxlength: 12,
     unique: true,
   },
-  // email: {
-  //   type: String,
-  //   required: [true, "Please provide email"],
-  //   match: [
-  //     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-  //     "Please provide a valid email",
-  //   ],
-  //   unique: true,
-  // },
   password: {
     type: String,
     required: [true, "Please provide password"],
